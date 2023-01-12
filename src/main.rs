7ffba214fb73dd1c8da1aadcc797da29fc1384a1
vidurkhanal <vkhanal@caldwell.edu>
2023-01-12 15:40:51 -0500

    3rd quest

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -7,15 +10,21 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:5050").unwrap();
     //
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let data = "+PONG\r\n";
-                let mut buf = data.as_bytes();
-                stream.write(&mut buf).unwrap();
+                let mut buf = [0; 5];
+                let n = stream.read(&mut buf).unwrap();
+
+                let command = String::from_utf8_lossy(&buf[0..n]);
+
+                if command.starts_with("ping") {
+                    let mut res = "+pong\r\n".as_bytes();
+                    stream.write(&mut res).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
